6826a6794ed64d80c9a78deb1ed61c3b8d27113a
Yu-Shiou Willy Lin <43131774+Willy624@users.noreply.github.com>
2021-01-04 20:52:56 -0600

    Update permutation_importance.R

--- permutation_importance.R
+++ permutation_importance.R
@@ -7,12 +7,17 @@
 # Output a variable importance row
 # In actual setting, we can create a data frame that store the variable importance result.
 # The column names of that data frame should be in the feature_list sequence so appending the row won't alter results.
+# Example formula and feature_list:
+f_covar_gam <- loglandprice_rm10med_cy_19 ~ landcat + land_source + te(normlng_cy, normlat_cy) + s(logarea) + s(landlevel_num) + s(plotratio_ll) + s(plotratio_ul)
+feature_list <- c("landcat", "logarea", "land_source", "landlevel_num", "plotratio_ll", "plotratio_ul", "geo_control")
+
+# The reason for using "geo_control" as a feature is because "te(normlng_cy, normlat_cy)" should be combined into one geographical control term.
+# When actually dealing with this, I combined the two columns and shuffled them together, then split them into lat, lng after shuffling.
 
 permutation_imp <- function(formula, data, feature_list, k = 30) {
 
   data <- data.frame(data)
   
-  # Bunch the lng and lat into geo_control and change accordingly
   model <- gam(formula, data = data)
   
   row <- vector(mode = "list", length = 0)
@@ -27,9 +32,11 @@ permutation_imp <- function(formula, data, feature_list, k = 30) {
     score <- 0
     new_data <- data
   
-    # Change the denominator accordingly would probably be better for each different feature?
-    # First have to find a way to get the edf for each term, probably need to split 
-    # between smooth terms and parameter terms
+    # In GAM feature_list, we have to distinguish between smoothing terms and parameter terms.
+    # Parameter terms effective degree of freedom will appear in pTerms (in a specific order, so we need to be careful)
+    # Smoothing terms effective degree of freedom will appear in s.table under the row_index of "s(...)"
+    # The reason to care about edf is because when calculating adjusted R-squares edf matters,
+    # and if we shuffle one column of observations, we should not take that columns edf into account.
     if (i == "landcat") {
       adj_df <- summary(model)$pTerms.df[[1]]
       denominator <- var(data$loglandprice_rm10med_cy_19 - mean(data$loglandprice_rm10med_cy_19))*(model$df.residual + adj_df)
@@ -49,6 +56,7 @@ permutation_imp <- function(formula, data, feature_list, k = 30) {
       }
     }
     
+    # This part
     if (i == "geo_control") {
       for (j in 1:k) {
         new_data <- new_data %>%
@@ -84,6 +92,6 @@ permutation_imp <- function(formula, data, feature_list, k = 30) {
     imp_var <- original_score - score
     row <- list.append(row, imp_var)
   }
-  result[nrow(result)+1, ] <- row
-  return(result)
+  
+  return(row)
 }
